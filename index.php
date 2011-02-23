88200b39e8ac259a5588497bd71aecd32ea2a65c
Cal Henderson <cal@iamcal.com>
2011-02-22 17:25:33 -0800

    readme and index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Glitch OAuth 2 Demos</title>
+</head>
+<body>
+
+<div style="width: 600px; margin: 0 auto;">
+
+	<h1>Glitch OAuth 2 Demos</h1>
+
+	<p>These demos show how to implement against the current Glitch OAuth 2.13 API.</p>
+
+	<p>Glitch currently supports server-side (<code>response_type=code</code>) and client-side (<code>response_type=token</code>) authentication.</p>
+
+	<p>The demos:</p>
+
+	<ul>
+
+		<li> <a href="./oauth-server/">Server-side demo</a> </li>
+		<li> <a href="./oauth-client/">Client-side demo</a> </li>
+	</ul>
+
+	<p>The source code for these demos is available on <a href="https://github.com/iamcal/glitch-oauth-demos">GitHub</a>.</p>
+
+</div>
+
+</body>
+</html>
